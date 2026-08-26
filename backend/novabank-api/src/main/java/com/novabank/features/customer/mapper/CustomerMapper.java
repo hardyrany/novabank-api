b5a5fb216dcadeb80856b2ce69b3fd2af35a9496
Hardy Fernandes <hardymtf@gmail.com>
2026-08-25 23:00:13 -0100

    feat: add CustomerMapper.toEntity
    - Add toEntity method for converting CustomerRequest to Customer
    - Centralizes entity creation from request DTO

--- backend/novabank-api/src/main/java/com/novabank/features/customer/mapper/CustomerMapper.java
+++ backend/novabank-api/src/main/java/com/novabank/features/customer/mapper/CustomerMapper.java
@@ -2,7 +2,27 @@ package com.novabank.features.customer.mapper;
 
 import org.springframework.stereotype.Component;
 
+import com.novabank.features.customer.dto.CustomerRequest;
+import com.novabank.features.customer.entity.Customer;
+
 @Component
 public class CustomerMapper {
 
+    public Customer toEntity(CustomerRequest request) {
+
+        Customer customer = new Customer();
+
+        customer.setFirstName(request.getFirstName());
+        customer.setMiddleName(request.getMiddleName());
+        customer.setLastName(request.getLastName());
+        customer.setEmail(request.getEmail());
+        customer.setPhone(request.getPhone());
+        customer.setDocumentNumber(request.getDocumentNumber());
+        customer.setDocumentType(request.getDocumentType());
+        customer.setBirthDate(request.getBirthDate());
+        customer.setAddress(request.getAddress());
+
+        return customer;
+
+    }
 }
