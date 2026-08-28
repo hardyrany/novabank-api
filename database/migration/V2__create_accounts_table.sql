dec179dd6034f5556c09e89ff68b4a85cf76904a
Hardy Fernandes <hardymtf@gmail.com>
2026-08-28 12:13:57 -0100

    feat: add accounts schema
    - Create accounts schema with IF NOT EXISTS

---
+++ database/migration/V2__create_accounts_table.sql
@@ -0,0 +1,5 @@
+-- V2__create_accounts_table.sql
+-- Description: Create accounts schema and table for NovaBank
+
+-- 1. Criar o schema accounts (se não existir)
+CREATE SCHEMA IF NOT EXISTS accounts;
