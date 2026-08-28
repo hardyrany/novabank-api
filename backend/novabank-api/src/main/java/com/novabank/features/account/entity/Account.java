f5bc97d074f41a1e87ee88e141be1853f2a0f841
Hardy Fernandes <hardymtf@gmail.com>
2026-08-28 12:47:44 -0100

    feat: add Account entity structure
    - Add @Entity and @Table annotations
    - Map to accounts table in accounts schema

---
+++ backend/novabank-api/src/main/java/com/novabank/features/account/entity/Account.java
@@ -0,0 +1,10 @@
+package com.novabank.features.account.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "accounts", schema = "accounts")
+public class Account {
+
+}
