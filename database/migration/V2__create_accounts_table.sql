c28e84340e7d4aa736002c58b06f88bcfab908e5
Hardy Fernandes <hardymtf@gmail.com>
2026-08-28 12:21:40 -0100

    feat: add accounts table structure
    - Create accounts.accounts table with all fields
    - Add CHECK constraint for account types (CHECKING, SAVINGS)
    - Add unique constraint for account_number

--- database/migration/V2__create_accounts_table.sql
+++ database/migration/V2__create_accounts_table.sql
@@ -3,3 +3,18 @@
 
 -- 1. Criar o schema accounts (se não existir)
 CREATE SCHEMA IF NOT EXISTS accounts;
+
+-- 2. Criar a tabela dentro do schema accounts
+CREATE TABLE IF NOT EXISTS accounts.accounts (
+    id BIGSERIAL PRIMARY KEY,
+    customer_id BIGINT NOT NULL,
+    account_Number VARCHAR(20) UNIQUE NOT NULL,
+    account_type VARCHAR(20) NOT NULL CHECK (
+        account_type IN ('CHECKING', 'SAVINGS')
+    ),
+    balance DECIMAL(19, 2) DEFAULT 0.00,
+    currency VARCHAR(3) DEFAULT 'USD',
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
