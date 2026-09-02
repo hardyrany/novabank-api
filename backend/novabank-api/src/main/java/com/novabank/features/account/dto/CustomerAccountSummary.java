c6309b2f1d053e53a4ccd0bb96cb5149dc86c357
Hardy Fernandes <hardymtf@gmail.com>
2026-09-02 21:31:15 -0100

    feat: add active/inactive account counts to CustomerAccountSummary
    - Add activeAccounts field
    - Add inactiveAccounts field
    - Update constructor to include new fields
    - Update getters and setters

--- backend/novabank-api/src/main/java/com/novabank/features/account/dto/CustomerAccountSummary.java
+++ backend/novabank-api/src/main/java/com/novabank/features/account/dto/CustomerAccountSummary.java
@@ -7,13 +7,21 @@ public class CustomerAccountSummary {
     private Long customerId;
     private BigDecimal totalBalance;
     private int totalAccounts;
+    private int activeAccount;
+    private int inactiveAccount;
 
+    public CustomerAccountSummary() {
 
-    public CustomerAccountSummary(Long customerId, BigDecimal totalBalance, int totalAccounts) {
+    }
+
+    public CustomerAccountSummary(Long customerId, BigDecimal totalBalance, int totalAccounts,
+            int activateAccount, int inactiveAccount) {
 
         this.customerId = customerId;
         this.totalBalance = totalBalance;
         this.totalAccounts = totalAccounts;
+        this.activeAccount = activateAccount;
+        this.inactiveAccount = inactiveAccount;
 
     }
 
@@ -22,29 +30,40 @@ public class CustomerAccountSummary {
         return customerId;
     }
 
-
     public void setCustomerId(Long customerId) {
         this.customerId = customerId;
     }
-
 
     public BigDecimal getTotalBalance() {
         return totalBalance;
     }
 
-
     public void setTotalBalance(BigDecimal totalBalance) {
         this.totalBalance = totalBalance;
     }
-
 
     public int getTotalAccounts() {
         return totalAccounts;
     }
 
-
     public void setTotalAccounts(int totalAccounts) {
         this.totalAccounts = totalAccounts;
+    }
+
+    public int getActiveAccount() {
+        return activeAccount;
+    }
+
+    public void setActiveAccount(int activeAccount) {
+        this.activeAccount = activeAccount;
+    }
+
+    public int getInactiveAccount() {
+        return inactiveAccount;
+    }
+
+    public void setInactiveAccount(int inactiveAccount) {
+        this.inactiveAccount = inactiveAccount;
     }
 
 }
