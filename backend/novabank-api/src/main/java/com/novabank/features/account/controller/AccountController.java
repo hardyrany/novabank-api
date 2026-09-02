a78101dc84a0e711a2430c750baeb649e84b1ac8
Hardy Fernandes <hardymtf@gmail.com>
2026-09-02 12:40:25 -0100

    feat: add POST /accounts endpoint
    - Add createAccount method with @PostMapping
    - Receive AccountRequest as @RequestBody
    - Convert to Account using AccountMapper
    - Call AccountService.createAccount
    - Convert response to AccountResponse
    - Return 201 CREATED status

--- backend/novabank-api/src/main/java/com/novabank/features/account/controller/AccountController.java
+++ backend/novabank-api/src/main/java/com/novabank/features/account/controller/AccountController.java
@@ -1,7 +1,14 @@
 package com.novabank.features.account.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.novabank.features.account.dto.AccountRequest;
+import com.novabank.features.account.dto.AccountResponse;
+import com.novabank.features.account.entity.Account;
 import com.novabank.features.account.mapper.AccountMapper;
 import com.novabank.features.account.service.AccountService;
 
@@ -16,6 +23,18 @@ public class AccountController {
 
         this.accountService = accountService;
         this.accountMapper = accountMapper;
+
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountResponse> createAccount(
+            @RequestBody AccountRequest accountRequest) {
+
+        Account account = accountMapper.toEntity(accountRequest);
+        Account savedAccount = accountService.createAccount(account);
+        AccountResponse accountResponse = accountMapper.toResponse(savedAccount);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(accountResponse);
 
     }
 
