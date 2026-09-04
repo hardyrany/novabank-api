d6eaec0b43c66e8e4f8124164388f9b1e430d345
Hardy Fernandes <hardymtf@gmail.com>
2026-09-04 19:12:16 -0100

    feat: add TransactionRepository structure
    - Create TransactionRepository interface
    - Extend JpaRepository<Transaction, Long>
    - Add @Repository annotation

---
+++ backend/novabank-api/src/main/java/com/novabank/features/transaction/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.novabank.features.transaction.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import jakarta.transaction.Transaction;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
