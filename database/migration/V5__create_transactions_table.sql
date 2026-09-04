4e38afff250c49248054c076902503ec53556f05
Hardy Fernandes <hardymtf@gmail.com>
2026-09-04 16:41:15 -0100

    feat: create transactions table
    - Create transactions table with id, account_id, transaction_type, amount
    - Add foreign key to accounts table
    - Add CHECK constraint for valid transaction types

--- database/migration/V5__create_transactions_table.sql
+++ database/migration/V5__create_transactions_table.sql
@@ -2,3 +2,20 @@
 -- Passo 1: Criar apenas o schema
 
 CREATE SCHEMA IF NOT EXISTS transactions;
+
+-- Passo 2: Criar a tabela transactions
+CREATE TABLE IF NOT EXISTS transactions.transactions (
+    id BIGSERIAL PRIMARY KEY,
+    account_id BIGINT NOT NULL,
+    transaction_type VARCHAR(20) NOT NULL,
+    amount DECIMAL(19, 2) NOT NULL,
+    CONSTRAINT fk_transactions_account FOREIGN KEY (account_id) REFERENCES accounts.accounts (id),
+    CONSTRAINT chk_transaction_type CHECK (
+        transaction_type IN (
+            'DEPOSIT',
+            'WITHDRAW',
+            'TRANSFER_IN',
+            'TRANSFER_OUT'
+        )
+    )
+);
