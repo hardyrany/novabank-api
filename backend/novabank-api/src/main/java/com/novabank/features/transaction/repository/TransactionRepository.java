bd7c169de887c172b455067776cd33560664b39c
Hardy Fernandes <hardymtf@gmail.com>
2026-09-04 19:18:17 -0100

    feat: add findByAccountIdAndTransactionType method
    - Add method to find transactions by account ID and transaction type
    - Order results by creation date descending
    - Query generated automatically from method name

--- backend/novabank-api/src/main/java/com/novabank/features/transaction/repository/TransactionRepository.java
+++ backend/novabank-api/src/main/java/com/novabank/features/transaction/repository/TransactionRepository.java
@@ -3,10 +3,14 @@ package com.novabank.features.transaction.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.novabank.features.transaction.enums.TransactionType;
 import jakarta.transaction.Transaction;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     List<Transaction> findByAccountIdOrderByCreatedAtDesc(Long accountId);
+
+    List<Transaction> findByAccountIdAndTransactionTypeOrderByCreatedAtDesc(Long accountId,
+            TransactionType transactionType);
 }
