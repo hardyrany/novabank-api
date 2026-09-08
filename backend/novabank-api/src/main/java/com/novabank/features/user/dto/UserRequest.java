79cc6029fe735de3f82d5d07205bf2ba20746eda
Hardy Fernandes <hardymtf@gmail.com>
2026-09-08 08:18:41 -0100

    feat: add constructors and accessors to UserRequest
    - Add default and parameterized constructors
    - Add getters and setters for all fields

--- backend/novabank-api/src/main/java/com/novabank/features/user/dto/UserRequest.java
+++ backend/novabank-api/src/main/java/com/novabank/features/user/dto/UserRequest.java
@@ -9,4 +9,36 @@ public class UserRequest {
     private String password;
     private Set<Role> roles;
 
+    public UserRequest() {}
+
+    public UserRequest(String email, String password, Set<Role> roles) {
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
 }
