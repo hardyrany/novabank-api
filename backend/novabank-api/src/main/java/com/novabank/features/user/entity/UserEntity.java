402090953268b836da7439e850605b7c899c11dd
Hardy Fernandes <hardymtf@gmail.com>
2026-09-08 07:47:46 -0100

    feat: add constructors and accessors to User entity
    - Add default and parameterized constructors
    - Add getters and setters for all fields

--- backend/novabank-api/src/main/java/com/novabank/features/user/entity/UserEntity.java
+++ backend/novabank-api/src/main/java/com/novabank/features/user/entity/UserEntity.java
@@ -23,4 +23,36 @@ public class UserEntity {
     @Column(nullable = false)
     private String password;
 
+    public UserEntity() {}
+
+    public UserEntity(UUID id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
