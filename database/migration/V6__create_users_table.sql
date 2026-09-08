01fb68ed535d5d7956edf6888be9c53a0159e485
Hardy Fernandes <hardymtf@gmail.com>
2026-09-08 07:33:11 -0100

    feat: create user_roles table
    - Create user_roles table for many-to-many roles
    - Add composite primary key (user_id, role)
    - Add foreign key to users with ON DELETE CASCADE
    - Add CHECK constraint for valid roles (ADMIN, USER, SUPPORT)

--- database/migration/V6__create_users_table.sql
+++ database/migration/V6__create_users_table.sql
@@ -12,3 +12,14 @@ CREATE TABLE IF NOT EXISTS users.users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+-- Passo 1.3: Criar tabela user_roles
+CREATE TABLE IF NOT EXISTS users.user_roles (
+    user_is UUID NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    PRIMARY KEY (user_id, role),
+    REFERENCES users.users (id) ON DELETE CASCADE,
+    CONSTRAINT chk_user_role CHECK (
+        role IN ('ADMIN', 'USER', 'SUPPORT')
+    )
+);
