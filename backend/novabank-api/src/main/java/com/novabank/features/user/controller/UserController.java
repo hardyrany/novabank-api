d615a45194bc76b1db8dbc7b9234b9a364fbe7b3
Hardy Fernandes <hardymtf@gmail.com>
2026-09-08 10:59:56 -0100

    feat: add POST /users endpoint
    - Add createUser method with @PostMapping
    - Receive UserRequest as @RequestBody
    - Call UserService.createUser
    - Return 201 CREATED status

--- backend/novabank-api/src/main/java/com/novabank/features/user/controller/UserController.java
+++ backend/novabank-api/src/main/java/com/novabank/features/user/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.novabank.features.user.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.novabank.features.user.dto.UserRequest;
+import com.novabank.features.user.dto.UserResponse;
 import com.novabank.features.user.mapper.UserMapper;
 import com.novabank.features.user.service.UserService;
 
@@ -15,5 +21,11 @@ public class UserController {
     public UserController(UserService userService, UserMapper userMapper) {
         this.userService = userService;
         this.userMapper = userMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest request) {
+        UserResponse response = userService.createUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
