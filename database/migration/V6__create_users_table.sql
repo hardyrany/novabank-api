f8027576104498fd3dae45160a9db571f6d81fe1
Hardy Fernandes <hardymtf@gmail.com>
2026-09-08 07:28:39 -0100

    feat: create users table
    - Create users table with UUID id, email, password, active
    - Add created_at and updated_at timestamps
    - Set email as unique and not null

--- database/migration/V6__create_users_table.sql
+++ database/migration/V6__create_users_table.sql
@@ -2,3 +2,13 @@
 -- Passo 1.1: Criar apenas o schema
 
 CREATE SCHEMA IF NOT EXISTS users;
+
+-- Passo 1.2: Criar tabela users
+CREATE TABLE IF NOT EXISTS users.users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    active BOOLEAN DEFAULT TRUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
