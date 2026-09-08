3d4a45888122d97700bda24da769a531ad30d425
Hardy Fernandes <hardymtf@gmail.com>
2026-09-08 10:14:57 -0100

    feat: implement toEntity method in UserMapper
    - Convert UserRequest to UserEntity
    - Set email, password and roles
    - Set active to true by default

---
+++ backend/novabank-api/src/main/java/com/novabank/features/user/UserMapper.java
@@ -0,0 +1,21 @@
+package com.novabank.features.user;
+
+import org.springframework.stereotype.Component;
+import com.novabank.features.user.dto.UserRequest;
+import com.novabank.features.user.entity.User;
+
+@Component
+public class UserMapper {
+
+    public User toEntity(UserRequest userRequest) {
+
+        User user = new User();
+
+        user.setEmail(userRequest.getEmail());
+        user.setPassword(userRequest.getPassword());
+        user.setRoles(userRequest.getRoles());
+        user.setIsActive(true);
+
+        return user;
+    }
+}
