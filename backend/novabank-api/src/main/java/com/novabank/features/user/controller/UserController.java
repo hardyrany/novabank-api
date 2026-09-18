2b6e96490a5a316b710cd6310cda9bd6ff3cfa11
Hardy Fernandes <hardymtf@gmail.com>
2026-09-18 21:51:21 -0100

    feat: add GET /api/v1/users endpoint
    - List all users
    - Delegates to UserService.getAllUsers

--- backend/novabank-api/src/main/java/com/novabank/features/user/controller/UserController.java
+++ backend/novabank-api/src/main/java/com/novabank/features/user/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.novabank.features.user.controller;
 
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.GetExchange;
 import com.novabank.features.user.dto.UserRequest;
 import com.novabank.features.user.dto.UserResponse;
 import com.novabank.features.user.mapper.UserMapper;
@@ -27,5 +29,10 @@ public class UserController {
     public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest request) {
         UserResponse response = userService.createUser(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetExchange
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 }
