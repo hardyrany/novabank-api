14ef71dd3e01af242d7594fba67545ddc9c3d293
Hardy Fernandes <hardymtf@gmail.com>
2026-09-22 11:58:47 -0100

    chore: remove unused GetExchange import from UserController
    - Leftover from previous implementation attempt
    - No functional change

--- backend/novabank-api/src/main/java/com/novabank/features/user/controller/UserController.java
+++ backend/novabank-api/src/main/java/com/novabank/features/user/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.GetExchange;
 import com.novabank.features.user.dto.UserRequest;
 import com.novabank.features.user.dto.UserResponse;
 import com.novabank.features.user.mapper.UserMapper;
